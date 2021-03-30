a3fba227590bdb4abe8b707db2e307b55aa800cb
Ty Richardson <ty.richardson@gmail.com>
2021-03-29 20:05:31 -0500

    Pig Game complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+// Adding and removing CSS Classes is basic for DOM manipulation
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+// console.log(btnsShowModal);
+const openModal = function () {
+  console.log(`Open modal button clicked`);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+// Both declared functions and function expressions work as callbacks
+const closeModal = function () {
+  console.log(`Close modal button clicked or Escape key pressed.`);
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+function overlayCloseModal() {
+  console.log(`Overlay close modal clicked.`);
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+// add EventListener/Handler to Show Modal... buttons
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+// add EventListener/Handler to Close Modal button w/ func expression
+btnCloseModal.addEventListener('click', closeModal);
+// add EventListener/Handler to Overlay close modal w/ declared func
+overlay.addEventListener('click', overlayCloseModal);
+// Keyboard Events are Global Events: keydown, keypress, keyup
+document.addEventListener('keydown', function (e) {
+  // console.log(e);
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,90 @@
 'use strict';
+// select elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore, activePlayer, scores, gameOver;
+
+init();
+
+// used to initialize game and to reset it to default state
+function init() {
+  currentScore = 0;
+  activePlayer = 0;
+  gameOver = false;
+  scores = [0, 0];
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  activePlayer = activePlayer == 0 ? 1 : 0;
+  currentScore = 0;
+};
+
+// roll the dice
+btnRoll.addEventListener('click', function () {
+  if (gameOver) return;
+  // generate random dice number
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // display the dye image
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+  // if dice != 1, add value to current score
+  if (dice != 1) {
+    currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    // switch players
+    switchPlayer();
+  }
+});
+
+// hold the score
+btnHold.addEventListener('click', function () {
+  if (gameOver) return;
+  // add current score to player Score
+  scores[activePlayer] += currentScore;
+  currentScore = 0;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // WINNER?
+  if (scores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    diceEl.classList.add('hidden');
+    gameOver = true;
+  }
+  // switch players
+  switchPlayer();
+});
+
+// reset game
+btnNew.addEventListener('click', init);
